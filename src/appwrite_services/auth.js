8f1797a627a98f59abc907931ad5f850eca19c1b
YashWagh9920 <yashwagh860@gmail.com>
2024-10-12 14:12:24 +0530

    google auth done

--- src/appwrite_services/auth.js
+++ src/appwrite_services/auth.js
@@ -1,63 +1,81 @@
 import config from "../config/config";
 
-import { Client, Account, ID } from "appwrite";
+import { Client, Account, ID, OAuthProvider } from "appwrite";
 
 class AuthService {
-    client =  new Client() ;
-    account;
+  client = new Client();
+  account;
 
-    constructor(){
-      this.client.setEndpoint(config.appwriteurl ).setProject(config.projectid );
-      this.account = new Account(this.client)
-    }
-    async createAccount({email,password,name}){
-      try {
-        const userAccount = await this.account.create(ID.unique(),email,password,name);
-         if(userAccount){
-          return this.login({email,password})
-         }
-         else{
-          return userAccount;
-         }
-      } 
-      catch (error) {
-       console.log(error);
-       
+  constructor() {
+    this.client.setEndpoint(config.appwriteurl).setProject(config.projectid);
+    this.account = new Account(this.client)
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        return this.login({ email, password })
+      }
+      else {
+        return userAccount;
       }
     }
-
-    async login ({email,password}){
-     try {
-      return await this.account.createEmailPasswordSession(email,password);
-     } 
-     catch (error) {
+    catch (error) {
       console.log(error);
-      
-     }
-    }
 
-    async getCurrentUser(){
-     try {
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  async getCurrentUser() {
+    try {
       return await this.account.get();
-     }
-      catch (error) {
-      console.log(error);
-      
-     }
-     return null ;
     }
+    catch (error) {
+      console.log(error);
 
-    async logout(){
-     try {
-        await this.account.deleteSessions();
-     } 
-     catch (error) {
-      console.log(error);
-      
-     }
     }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  // login with google
+  async LoginWithGoogle() {
+
+  const successURL = window.location.origin + '/';
+  const failureURL = window.location.origin + '/login';
+
+    try {
+      return this.account.createOAuth2Session(
+        OAuthProvider.Google,
+        successURL,
+        failureURL,
+      );
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 const authService = new AuthService();
 
-export default authService ;
+export default authService;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import {Button,Input,Logo} from"./index"
 import { useDispatch } from 'react-redux'
 import authService from '../appwrite_services/auth'
 import { useForm } from 'react-hook-form'
+import googleLogo from '../assets/images/google-logo.png'
 
 function Login() {
     const navigate = useNavigate()
@@ -16,9 +17,7 @@ function Login() {
         // Check if the user is already logged in
         const checkSession = async () => {
             try {
-                const user = await authService.getCurrentUser();
-                console.log(user);
-                
+                const user = await authService.getCurrentUser();                
                 if (user) {
                     navigate("/");
                 }
@@ -47,55 +46,84 @@ function Login() {
             setError(error.message)
         }
     }
+
+    const loginWithGoogle = async () => {
+        setError("")
+        try {            
+         const session = await authService.LoginWithGoogle()
+         if(session) {
+            //   const userData = await authService.getCurrentUser();
+            //   if(userData) {
+            //     dispatch(authLogin(userData))
+                navigate("/")
+            // }
+            }
+            else{
+                navigate("/login")
+            }
+        } 
+        catch (error) {
+            setError(error.message)
+        }
+    }
+
+
   return (
-    <div
-    className='flex items-center justify-center w-full'
-    >
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
-        <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
-                        <Logo width="100%" />
-                    </span>
+    <div className='flex items-center justify-center w-full'>
+    <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+      <div className="mb-2 flex justify-center">
+        <span className="inline-block w-full max-w-[100px]">
+          <Logo width="100%" />
+        </span>
+      </div>
+      <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+      <p className="mt-2 text-center text-base text-black/60">
+        Don&apos;t have any account?&nbsp;
+        <Link
+          to="/signup"
+          className="font-medium text-primary transition-all duration-200 hover:underline"
+        >
+          Sign Up
+        </Link>
+      </p>
+      {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+      <form onSubmit={handleSubmit(login)} className='mt-8'>
+        <div className='space-y-5'>
+          <Input
+            label="Email: "
+            placeholder="Enter your email"
+            type="email"
+            {...register("email", {
+              required: true,
+              validate: {
+                matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                  "Email address must be a valid address",
+              }
+            })}
+          />
+          <Input
+            label="Password: "
+            placeholder="Enter your password"
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 8
+            })}
+          />
+          <Button type="submit" className="w-full bg-primary text-white">Sign In</Button>
+         <span className='flex justify-center items-center text-xl font-bold'> <span>OR</span></span>
         </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-        <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
-                    <Link
-                        to="/signup"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
-                    >
-                        Sign Up
-                    </Link>
-        </p>
-        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)} className='mt-8'>
-            <div className='space-y-5'>
-                <Input
-                label = "Email: "
-                placeholder = "Enter your email"
-                type = "email"
-                {...register("email",{
-                    required: true,
-                    validate:{
-                         matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                        "Email address must be a valid address",
-                    }
-                })}
-                />
-                <Input
-                label = "Password: "
-                placeholder = "Enter your password"
-                type = "password"
-                {...register("password",{
-                    required: true,
-                    minLength: 8
-                })}
-                />
-                <Button type="submit" className="w-full bg-primary text-white">Sign In</Button>
-            </div>
-        </form>
-        </div>
+      </form>
+      <button
+            type="button"
+            className="w-full text-black border-black border-2 rounded-lg mt-4 bg-white/50 p-1"
+            onClick={() =>loginWithGoogle()}
+          >
+           <span className='flex justify-center items-center gap-2 font-semibold'> Continue with Google
+           <img className='h-7 w-7' src={googleLogo} alt="" /></span>
+          </button>
     </div>
+  </div>
   )
 }
 
